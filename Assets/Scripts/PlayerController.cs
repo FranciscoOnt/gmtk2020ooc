a58085f99a2ffe37b661e392578e4b7a44a58500
Francisco Ontiveros <jfov@outlook.es>
2020-07-10 23:56:25 -0500

    water recovery item

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,19 +10,32 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float waterDelay = .2f;
     private float waterTimer = 0f;
+    public float waterConsumptionRate = .2f;
     public Transform water;
     private Vector2 velocity;
     private bool isShotingWater = false;
     private Rigidbody2D rb;
+    private float waterTank = 100f;
+    public int life = 5;
+    public float flinchDelay = .5f;
+    public float flinchMultiply = 5f;
+    private bool flinching = false;
 
     void FixedUpdate()
     {
-        rb.velocity = velocity * moveSpeed;
+        if (!flinching)
+            rb.velocity = velocity * moveSpeed;
     }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(510, 10, 150, 20), "Water: " + waterTank.ToString());
+        GUI.Label(new Rect(510, 25, 150, 20), "Life: " + life.ToString());
     }
 
     private IEnumerator ShotWaterRoutine()
@@ -37,8 +50,23 @@ public class PlayerController : MonoBehaviour
 
     public void ShotWater()
     {
-        Transform drop = Instantiate(water, waterOrigin.transform.position, Quaternion.identity);
-        drop.GetComponent<Water>().SetVelocity(crossHair.GetCrosshaiPosition() - transform.position);
+        if (waterTank > 0)
+        {
+            waterTank -= waterConsumptionRate;
+            Transform drop = Instantiate(water, waterOrigin.transform.position, Quaternion.identity);
+            drop.GetComponent<Water>().SetVelocity(crossHair.GetCrosshaiPosition() - transform.position);
+        }
+        else
+        {
+            waterTank = 0f;
+        }
+    }
+
+    public void getWater(float amount)
+    {
+        waterTank += amount;
+        if (waterTank > 100f)
+            waterTank = 100f;
     }
 
     public void SetDirection(Vector2 direction)
@@ -59,5 +87,32 @@ public class PlayerController : MonoBehaviour
     {
         crossHair.SetPosition(direction);
         waterOrigin.SetPosition(direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Tree"))
+        {
+            if (other.transform.GetComponent<Tree>().burning)
+            {
+                Damage();
+                Vector3 flinchdir = Vector3.Normalize((Vector2)transform.position - (Vector2)other.transform.position);
+                StartCoroutine("Flinch", flinchdir);
+            }
+        }
+    }
+
+
+    public IEnumerator Flinch(Vector3 direction)
+    {
+        flinching = true;
+        rb.velocity = direction * flinchMultiply;
+        yield return new WaitForSeconds(flinchDelay);
+        flinching = false;
+    }
+
+    public void Damage()
+    {
+        life--;
     }
 }
---
+++ Assets/Scripts/WaterItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterItem : MonoBehaviour
+{
+    public float amount = 5f;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            other.transform.GetComponent<PlayerController>().getWater(amount);
+            Destroy(gameObject);
+        }
+    }
+}
